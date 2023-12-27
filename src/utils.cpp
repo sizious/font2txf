c4a58314d7fbdfc37ee66fada2d32cdd07202605
SiZiOUS <sizious@gmail.com>
2023-12-27 09:50:44 +0000

    Cleaning up a bit utils.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,44 +1,10 @@
-/* IP creator (makeip)
- *
- * Copyright (C) 2000, 2001, 2002, 2019, 2020 The KOS Team and contributors.
- * All rights reserved.
- *
- * This code was contributed to KallistiOS (KOS) by Andress Antonio Barajas
- * (BBHoodsta). It was originally made by Marcus Comstedt (zeldin). Some
- * portions of code were made by Andrew Kieschnick (ADK/Napalm). Heavily
- * updated by SiZiOUS. Bootstrap replacement (IP.TMPL) was made by Jacob
- * Alberty (LiENUS).
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "utils.h"
 
 int g_verbose = 0;
 
 char *g_program_name;
 
-void
-program_name_initialize(char *argv0)
+void program_name_initialize(char *argv0)
 {
   char *result = basename(argv0);
   char *buf = strrchr(result, '.');
@@ -49,14 +15,12 @@ program_name_initialize(char *argv0)
   g_program_name = strdup(result);
 }
 
-char *
-program_name_get()
+char * program_name_get()
 {
   return g_program_name;
 }
 
-void
-program_name_finalize()
+void program_name_finalize()
 {
   free(g_program_name);	
 }
